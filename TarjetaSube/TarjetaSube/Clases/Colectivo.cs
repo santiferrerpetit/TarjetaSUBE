ea976681dd71b8c9c78ce90fd75f7abf1b9b8f4b
ManuGave <119437824+ManuGave@users.noreply.github.com>
2026-09-15 16:05:36 -0300

    Clase y tests de colectivo

--- TarjetaSube/TarjetaSube/Clases/Colectivo.cs
+++ TarjetaSube/TarjetaSube/Clases/Colectivo.cs
@@ -5,8 +5,27 @@ public class Colectivo
     public int Id { get; set; }
     public string Linea { get; set; } = string.Empty;
 
+    private const int Tarifa = 1580;
     public Boleto PagarCon(Tarjeta tarjeta)
     {
-        throw new NotImplementedException();
+        if (tarjeta.Saldo >= Tarifa)
+        {
+            tarjeta.Saldo -= Tarifa;
+
+            Console.WriteLine("Pago aceptado, disfrute su viaje.");
+            Console.WriteLine($"Su saldo es de {tarjeta.Saldo}");
+            return new Boleto
+            {
+                Monto = Tarifa,
+                Tarjeta = tarjeta,
+                Colectivo = this
+            };
+        }
+        else
+        {
+        throw new ArgumentException("Saldo insuficiente, cargue la tarjeta e intente de nuevo");    
+        }
+
+        
     }
 }
